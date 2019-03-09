5160fac41b908d1e5ba21d79e0804835276af9b8
lio8n8 <lio8n8@gmail.com>
2019-03-09 14:11:42 +0200

    Api docs for products

--- routes/products.js
+++ routes/products.js
@@ -5,6 +5,60 @@ const checkAuth = require('../middleware/checkAuth');
 const validateObjectId = require('../middleware/validateObjectId');
 const configs = require('../configs');
 
+/**
+ * @apiDefine ApiProductSuccessResponse
+ * @apiSuccessExample {json} Success
+ *    HTTP/1.1 200 OK
+ *    {
+ *      "price": 420,
+ *      "isOnline": false,
+ *      "_id": "5c83a5056f5a5b3e96db07cf",
+ *      "name": "Laudantium totam",
+ *      "title": "Laudantium totam in magnam quisquam cupiditate id amet repellat.",
+ *      "description":"Possimus recusandae facere quia architecto quaerat. Recusandae excepturi iusto nobis dicta quia. Laudantium totam 
+ *                     et ut quae aliquid. Dolorem est ipsa. Quam beatae unde et inventore.","shortDescription":"Occaecati dolorem ab et 
+ *                     suscipit quia. Quis et suscipit atque neque asperiores eius ut non minus. Rem nisi dolor corrupti tenetur accusamus.
+ *                     Rem ducimus sit aliquid.",
+ *      "createdBy": "5c83a5056f5a5b3e96db07ce"
+ *    }
+ */
+
+/**
+ * @apiDefine ApiProductSuccessFields
+ * 
+ * @apiSuccess {String} _id Product id
+ * @apiSuccess {Number} price Product price
+ * @apiSuccess {String} name Produc name
+ * @apiSuccess {String} title Produc title
+ * @apiSuccess {String} description Product description
+ * @apiSuccess {String} createdBy Admin id
+ * @apiSuccess {Boolean} isOnline Default false
+ */
+
+/**
+ * @api {get} /api/products Get products
+ * @apiGroup Products
+ * 
+ * @apiSuccess {Array} Products Array of products
+ * @apiSuccessExample {json} Success
+ *    HTTP/1.1 200 OK
+ *    [{
+ *      "price": 420,
+ *      "isOnline": false,
+ *      "_id": "5c83a5056f5a5b3e96db07cf",
+ *      "name": "Laudantium totam",
+ *      "title": "Laudantium totam in magnam quisquam cupiditate id amet repellat.",
+ *      "description":"Possimus recusandae facere quia architecto quaerat. Recusandae excepturi iusto nobis dicta quia. Laudantium totam 
+ *                     et ut quae aliquid. Dolorem est ipsa. Quam beatae unde et inventore.","shortDescription":"Occaecati dolorem ab et 
+ *                     suscipit quia. Quis et suscipit atque neque asperiores eius ut non minus. Rem nisi dolor corrupti tenetur accusamus.
+ *                     Rem ducimus sit aliquid.",
+ *      "createdBy": "5c83a5056f5a5b3e96db07ce"
+ *    }]
+ * @apiErrorExample {json} Not found
+ *    HTTP/1.1 404 Not Found
+ * @apiErrorExample {json} Internal server error
+ *    HTTP/1.1 500 Internal Server Error
+ */
 router.get('/', async (req, res, next) => {
     try {
         const limit = (req.params.limit && req.params.limit <= configs.LIMIT) || configs.LIMIT;
@@ -17,6 +71,17 @@ router.get('/', async (req, res, next) => {
     }
 });
 
+/**
+ * @api {get} /api/products/:id Get product
+ * @apiGroup Products
+ * 
+ * @apiUse ApiProductSuccessFields
+ * @apiUse ApiProductSuccessResponse
+ * @apiErrorExample {json} Not found
+ *    HTTP/1.1 404 Not Found
+ * @apiErrorExample {json} Internal server error
+ *    HTTP/1.1 500 Internal Server Error
+ */
 router.get('/:id', async (req, res, next) => {
     try {
         const id = req.params.id;
@@ -28,6 +93,26 @@ router.get('/:id', async (req, res, next) => {
     }
 });
 
+/**
+ * @api {post} /api/products Create product
+ * @apiGroup Products
+ * @apiPermission admin
+ * @apiHeaderExample {json} Authorization:
+ *     {
+ *       "authorization": "Bearer {token}"
+ *     }
+ * @apiParam {Number} price Product price
+ * @apiParam {String} name Produc name
+ * @apiParam {String} title Produc title
+ * @apiParam {String} description Product description
+ * 
+ * @apiUse ApiProductSuccessFields
+ * @apiUse ApiProductSuccessResponse
+ * @apiErrorExample {json} Not found
+ *    HTTP/1.1 404 Not Found
+ * @apiErrorExample {json} Internal server error
+ *    HTTP/1.1 500 Internal Server Error
+ */
 router.post('/', checkAuth.admin, async (req, res, next) => {
     try {
         const data = req.body;
@@ -40,6 +125,26 @@ router.post('/', checkAuth.admin, async (req, res, next) => {
     }
 });
 
+/**
+ * @api {put} /api/products/:id Update product
+ * @apiGroup Products
+ * @apiPermission admin
+ * @apiHeaderExample {json} Authorization:
+ *     {
+ *       "authorization": "Bearer {token}"
+ *     }
+ * @apiParam {Number} price Product price
+ * @apiParam {String} name Produc name
+ * @apiParam {String} title Produc title
+ * @apiParam {String} description Product description
+ * 
+ * @apiUse ApiProductSuccessFields
+ * @apiUse ApiProductSuccessResponse
+ * @apiErrorExample {json} Not found
+ *    HTTP/1.1 404 Not Found
+ * @apiErrorExample {json} Internal server error
+ *    HTTP/1.1 500 Internal Server Error
+ */
 router.put('/:id', [validateObjectId, checkAuth.admin], async (req, res, next) => {
     try {
         const id = req.params.id;
@@ -52,6 +157,21 @@ router.put('/:id', [validateObjectId, checkAuth.admin], async (req, res, next) =
     }
 });
 
+/**
+ * @api {delete} /api/products/:id Delete product
+ * @apiGroup Products
+ * @apiPermission admin
+ * @apiHeaderExample {json} Authorization:
+ *     {
+ *       "authorization": "Bearer {token}"
+ *     }
+ * @apiSuccessExample {json} Success
+ *    HTTP/1.1 204 No Content
+ * @apiErrorExample {json} Not found
+ *    HTTP/1.1 404 Not Found
+ * @apiErrorExample {json} Internal server error
+ *    HTTP/1.1 500 Internal Server Error
+ */
 router.delete('/:id', [validateObjectId, checkAuth.admin], async (req, res, next) => {
     try {
         const id = req.params.id;
